0c4e174f5789945d3712c6051b4151885f1fcb08
JBrady <jbradysd@icloud.com>
2021-09-20 12:04:03 -0700

    Uncommenting all the code that needs to be reviewed

--- coding-concepts.js
+++ coding-concepts.js
@@ -7,8 +7,8 @@
 
 // --------------------1) What will this log?
 
-// var cohort = "Delta 2021"
-// console.log(cohort.split(""))
+var cohort = "Delta 2021"
+console.log(cohort.split(""))
 
 // a) Your answer: ["D", "e", "l", "t", "a", " ", "2", "0", "2", "1"]
 //    .split is a getter (accessor) method that converts a string into an array. This should return the string as a single array, and each letter will be put in its own index with each letter as its value.
@@ -19,10 +19,10 @@
 
 // --------------------2) What will this log?
 
-// const greeter = (name) => {
-//   `Hello, ${name}!`
-// }
-// console.log(greeter("LEARN Student"))
+const greeter = (name) => {
+  `Hello, ${name}!`
+}
+console.log(greeter("LEARN Student"))
 
 // a) Your answer: 'undefined'
 //    The function doesn't have a return keyword, and won't return anything.
@@ -37,8 +37,8 @@
 
 // --------------------3) What will this log?
 
-// var multBy2 = [4, 5, 6, 7, 8].map(value => value * 2)
-// console.log(multBy2)
+var multBy2 = [4, 5, 6, 7, 8].map(value => value * 2)
+console.log(multBy2)
 
 // a) Your answer: [8, 10, 12, 14, 16]
 //    This is declaring a variable named multBy2 and assigning it the value of an array. The .map() method returns each value of the array and takes 'value' as a parameter. Value is being multiplied by 2, so I think each index in the array will be multiplied by 2.
@@ -48,8 +48,8 @@
 
 // --------------------4) What will this log?
 
-// var oddsOnly = [11, 12, 13, 14, 15].filter(value => value % 2 === 0)
-// console.log(oddsOnly)
+var oddsOnly = [11, 12, 13, 14, 15].filter(value => value % 2 === 0)
+console.log(oddsOnly)
 
 // a) Your answer: [12, 14]
 //    This should return an array with only numbers that are divisible by 2.
@@ -59,15 +59,15 @@
 
 // --------------------5) What will this log?
 
-// class Learn {
-//   constructor(name){
-//     this.student = name,
-//     this.cohort = "Delta",
-//     this.year = 2021
-//   }
-// }
-// var learnStudent = new Learn("George")
-// console.log(learnStudent)
+class Learn {
+  constructor(name){
+    this.student = name,
+    this.cohort = "Delta",
+    this.year = 2021
+  }
+}
+var learnStudent = new Learn("George")
+console.log(learnStudent)
 
 // a) Your answer: ["George", "Delta", 2021] 
 //    The variable learnStudent is assigned to a new class called Learn. Learn takes in the parameter name, which is also the value of this.student key. Using the argument "George" will assign that value to this.student key resulting in the predicted output.
